ea4ad0fbbe6016bcb3f6e2bb47a2d011c5e57056
prefierolasoledad <karanbhatt9900@gmail.com>
2025-07-26 16:23:22 +0530

    server load balancing

--- server.js
+++ server.js
@@ -3,17 +3,33 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-const app = express();
+const cluster = require('cluster');
+const os = require('os');
 
-app.use(cors());
-app.use(express.json());
-// app.use('/uploads', express.static('uploads')); // to access uploaded files
-app.use('/register', require('./routes/registration'));
+const numCPUs = os.cpus().length;
 
-// Connect MongoDB
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error(err));
+if (cluster.isMaster) {
+    console.log(`Master process running on PID: ${process.pid}`);
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    cluster.on('exit', (worker) => {
+        console.log(`Worker ${worker.process.pid} died. Restarting...`);
+        cluster.fork();
+    });
+
+} else {
+    const app = express();
+
+    app.use(cors());
+    app.use(express.json());
+    app.use('/register', require('./routes/registration'));
+
+    mongoose.connect(process.env.MONGO_URI)
+        .then(() => console.log('MongoDB connected'))
+        .catch(err => console.error(err));
+
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Worker ${process.pid} running on port ${PORT}`));
+}
