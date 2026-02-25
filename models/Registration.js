907fd8edce1a02c617417057d52c0b5b543c30de
Karan Bhatt <104421276+prefierolasoledad@users.noreply.github.com>
2026-02-26 00:27:36 +0530

    Rename model from MentorshipRegistration to EventRegistration

--- models/Registration.js
+++ models/Registration.js
@@ -13,4 +13,4 @@ const registrationSchema = new mongoose.Schema({
     // resumeUrl: String, // Local path or cloud URL
 }, { timestamps: true });
 
-module.exports = mongoose.model('MentorshipRegistration', registrationSchema);
+module.exports = mongoose.model('EventRegistration', registrationSchema);
